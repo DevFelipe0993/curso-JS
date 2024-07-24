d5d2b02c6b90087658f1d9a130c21f56cfee8c04
Felipe Marques <162763543+DevFelipe0993@users.noreply.github.com>
2024-07-24 12:03:39 -0300

    ex JS Hora

---
+++ aula13ex/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = 'imagens/dia.png'
+        document.body.style.background = 'rgba(243, 162, 87, 0.877)'
+    } else if (hora >= 12 && hora <= 19) {
+        ///Boa tarde!
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = 'rgb(173, 115, 27)'
+    } else {
+        //Boa noite!
+        img.src = 'imagens/noite.png'
+        document.body.style.background = 'rgba(0, 0, 0, 0.74)'
+    }
+}
