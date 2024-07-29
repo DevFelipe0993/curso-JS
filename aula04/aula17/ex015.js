90f1ce24427eb7cf310f09aa3672142cc7df152c
Felipe Marques <162763543+DevFelipe0993@users.noreply.github.com>
2024-07-28 22:24:55 -0300

    att js

--- aula04/aula17/ex015.js
+++ aula04/aula17/ex015.js
@@ -1,3 +1,6 @@
-let num = []
-
-
+let num = [2,8,7,1,9,3,5]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
